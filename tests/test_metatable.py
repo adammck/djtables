4876eb4e90eee34ce80e9cc0496e39d58e66708a
adammck <adam.mckaig@gmail.com>
2010-04-20 00:29:12 -0400

    moved 'columns' to table options, to match models.

--- tests/test_metatable.py
+++ tests/test_metatable.py
@@ -6,17 +6,46 @@ from tables.table import MetaTable
 from tables.column import Column
 
 
+# unlike the real Options, this mock will accept ANY names
+class MockOptions(object):
+    def __init__(self, options):
+        if options is not None:
+            for name, value in options.__dict__.items():
+                setattr(self, name, value)
+
+
+def test_adds_table_options():
+    class TableNoMeta(object):
+        __metaclass__ = MetaTable
+        options_class = MockOptions
+
+    assert hasattr(TableNoMeta, '_meta')
+    assert isinstance(TableNoMeta._meta, MockOptions)
+
+    class TableWithMeta(object):
+        __metaclass__ = MetaTable
+        options_class = MockOptions
+
+        class Meta:
+            one = 1
+            two = 2
+
+    assert TableWithMeta._meta.one == 1
+    assert TableWithMeta._meta.two == 2
+
+
 def test_captures_column_attrs():
     class AnyColumnSubclass(Column):
         pass
 
     class AnyTable(object):
         __metaclass__ = MetaTable
+        options_class = MockOptions
         alpha = AnyColumnSubclass()
         beta  = Column()
 
     def check(name):
-        assert name in AnyTable.columns.keys()
+        assert name in AnyTable._meta.columns.keys()
         assert not hasattr(AnyTable, name)
 
     check('alpha')
@@ -26,10 +55,11 @@ def test_captures_column_attrs():
 def test_ignores_non_column_attrs():
     class AnyTable(object):
         __metaclass__ = MetaTable
+        options_class = MockOptions
         gamma = False
         delta = 999
 
-    assert len(AnyTable.columns) == 0
+    assert len(AnyTable._meta.columns) == 0
     assert AnyTable.gamma == False
     assert AnyTable.delta == 999
 
@@ -37,9 +67,10 @@ def test_ignores_non_column_attrs():
 def test_preserves_column_order():
     class AnyTable(object):
         __metaclass__ = MetaTable
+        options_class = MockOptions
         epsilon = Column()
         zeta    = Column()
         eta     = Column()
 
-    assert AnyTable.columns.keys() == [
+    assert AnyTable._meta.columns.keys() == [
         'epsilon', 'zeta', 'eta']
