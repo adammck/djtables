41cc7a7a37dabfe5ef8c6b82935757c7287c479b
adammck <adam.mckaig@gmail.com>
2010-05-14 02:40:48 -0400

    mocked Cell tests using fudge.
    i'm fed up with mox already. but i really like the look of fudge. it
    reminds me of mocha (although it's a bit more verbose).  so i'm going
    to take it for a spin in this project, where it can't do any harm.

--- tables/cell.py
+++ tables/cell.py
@@ -3,13 +3,22 @@
 
 
 class Cell(object):
+
+    """
+    This class represents a single table cell. It does not hold a value,
+    only references to the Column and Row which it intersects. The value
+    is fetched from the Row, and rendered by the Column.
+    """
+
     def __init__(self, column, row):
         self.column = column
         self.row = row
 
     def __unicode__(self):
-        return self.column.render(self.value)
+        """Return the rendered (via self.column) value of this cell."""
+        return unicode(self.column.render(self.value))
 
     @property
     def value(self):
+        """Return the value of this cell."""
         return getattr(self.row, self.column.name)
--- tests/test_cell.py
+++ tests/test_cell.py
@@ -2,26 +2,26 @@
 # vim: et ts=4 sw=4
 
 
+from fudge import Fake
 from tables.cell import Cell
 
 
-class MockColumn:
-    name = "a"
-
-    def render(self, x):
-        return x.upper()
-
-
-class MockRow:
-    a = "alpha"
-    b = "beta"
-
-
 def test_gets_value_from_row():
-    c = Cell(MockColumn(), MockRow())
-    assert c.value == "alpha"
+    column = Fake().has_attr(name="alpha")
+    row    = Fake().has_attr(alpha="aaa")
+    cell   = Cell(column, row)
+
+    assert cell.value == "aaa"
 
 
 def test_renders_via_column():
-    c = Cell(MockColumn(), MockRow())
-    assert unicode(c) == "ALPHA"
+    column = Fake().has_attr(name="beta")
+    row    = Fake().has_attr(beta="bbb")
+    cell   = Cell(column, row)
+
+    (column
+        .provides("render")
+        .with_args("bbb")
+        .returns("BBB"))
+
+    assert unicode(cell) == u"BBB"
