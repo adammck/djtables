33290b0bb5efb7ff91b8636e405dfa6de0956981
adammck <adam.mckaig@gmail.com>
2010-08-05 10:44:27 -0400

    fixed setup to include html templates, bumped version.

--- setup.py
+++ setup.py
@@ -7,14 +7,20 @@ from setuptools import setup
 
 setup(
     name="djtables",
-    version="0.1.0",
+    version="0.1.1",
     license="BSD",
+
+    install_requires = [
+        "django"
+    ],
+
+    package_dir={"": "lib"},
+    packages=["djtables"],
+    include_package_data=True,
 
     author="Adam Mckaig",
     author_email="adam.mckaig@gmail.com",
 
     description="Declarative HTML table builder for Django",
-    url="http://github.com/adammck/djtables",
-
-    package_dir={"": "lib"},
-    packages=["djtables"])
+    url="http://github.com/adammck/djtables"
+)
