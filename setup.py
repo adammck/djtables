d5d87fd41217701ff933fdf129e21463d58a16d4
adammck <adam.mckaig@gmail.com>
2010-08-05 02:31:07 -0400

    added setup.

---
+++ setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# vim: et ts=4 sw=4
+
+
+from setuptools import setup
+
+
+setup(
+    name="django-tables",
+    version="0.1.0",
+    license="CC0",
+
+    author="Adam Mckaig",
+    author_email="adam.mckaig@gmail.com",
+
+    description="Declarative HTML table builder for Django",
+    url="http://github.com/adammck/django-tables",
+    packages=["djangotables"])
