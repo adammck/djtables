30eac9aff59d617ba40170beab9db344154b785f
adammck <adam.mckaig@gmail.com>
2010-08-05 04:35:06 -0400

    fixed package_dir and license. oops.

--- setup.py
+++ setup.py
@@ -8,11 +8,13 @@ from setuptools import setup
 setup(
     name="djtables",
     version="0.1.0",
-    license="CC0",
+    license="BSD",
 
     author="Adam Mckaig",
     author_email="adam.mckaig@gmail.com",
 
     description="Declarative HTML table builder for Django",
     url="http://github.com/adammck/djtables",
-    packages=["lib/djtables"])
+
+    package_dir={"": "lib"},
+    packages=["djtables"])
